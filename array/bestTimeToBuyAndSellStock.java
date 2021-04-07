8acdf5a208097c8db3117e5c575ec6b41f609eba
yankeeguyu <yankeeguyu@gmail.com>
2021-04-07 14:07:24 +0800

    leetcode121

---
+++ array/bestTimeToBuyAndSellStock.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) return 0;
+        int n = prices.length;
+        int dp[][] = new int[n][2];
+        // dp[i][0] 买入或者持有
+        // dp[i][1] 卖出或者不持有
+        dp[0][0] = -prices[0];
+        dp[0][1] = 0;
+
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], -prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+        } 
+        return dp[n - 1][1];
+    }
+}
