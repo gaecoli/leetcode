122ccadc4234d0877391638bf12df0e955e2c572
yankeeguyu <yankeeguyu@gmail.com>
2021-04-07 23:42:13 +0800

    leetcode167

---
+++ array/twoSumIIinputArrayIsSorted.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        //二分查找，不是一种优质的解
+        for (int i = 0; i < numbers.length; ++i) {
+            int low = i + 1, high = numbers.length - 1;
+            while (low <= high) {
+                int mid = (high - low) / 2 + low;
+                if (numbers[mid] == target - numbers[i]) {
+                    return new int[]{i + 1, mid + 1};
+                } else if (numbers[mid] > target - numbers[i]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
