dfd762dd76e7fe931d61b15d3b093a61ae307286
yankeeguyu <yankeeguyu@gmail.com>
2021-04-11 12:37:48 +0800

    leetcode268

---
+++ array/missingNumber.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+        for (int i = 0; i <= nums.length; i++) {
+            if (!set.contains(i)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
